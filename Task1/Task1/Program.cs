8b11e2ef23ab16fb12535225179e54e683bfd3fb
My Name <myEmail@example.com>
2024-12-01 22:34:56 +0300

    коммит

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите ваше имя");
+            string name = Console.ReadLine();
+            Console.WriteLine("Введите ваш возраст");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите ваш рост");
+            double height = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Привет, {name}!");
+            Console.WriteLine($"\tВаш возраст: {age} лет");
+            Console.WriteLine($"\t\tВаш рост: {height} м");
+        }
+    }
+}
---
+++ Task2/Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число:");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите второе число:");
+            double b = Convert.ToDouble (Console.ReadLine());
+            double sum = a + b;
+            double raz = a - b;
+            double proiz = a * b;
+            double delen = a / b;
+
+            Console.WriteLine($"Сумма чисел:{sum}");
+            Console.WriteLine($"Произведение чисел:{proiz}");
+            Console.WriteLine($"Разность чисел:{raz}");
+            Console.WriteLine($"Частное чисел:{delen}");
+        }
+    }
+}
